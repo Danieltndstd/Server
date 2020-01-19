e46f2def12ed9d7196242066995a869f34552b88
Danieltndstd <https://github.com/leon-777/yhqp-server.git>
2020-01-19 17:14:25 +0800

    删除

--- Server/KernelEngine/zlreactor/ByteBuffer.cpp
+++
@@ -1,9 +0,0 @@
-#include "Stdafx.h"
-#include "ByteBuffer.h"
-namespace zl
-{
-	namespace net{
-		const char ByteBuffer::kCRLF[] = "\r\n";
-		const char ByteBuffer::kDoubleCRLF[] = "\r\n\r\n";
-	}
-}
